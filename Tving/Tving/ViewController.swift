f4b90d9e036cb680f3442bd337f8be8edb0aed08
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-04-23 12:55:00 +0900

    Initial Commit

---
+++ Tving/Tving/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tving
+//
+//  Created by 임재현 on 4/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hi")
+    }
+
+
+}
+
