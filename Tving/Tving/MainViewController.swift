79bc518838bb378f52e553cb37cfd6a100bc9319
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-05-02 16:48:40 +0900

    [feat] PagingTabBar,MainVC 상단 LogoView 부분 UI 작업

--- Tving/Tving/MainViewController.swift
+++ Tving/Tving/MainViewController.swift
@@ -10,8 +10,122 @@ import SnapKit
 
 final class MainViewController: UIViewController {
     
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "TVING")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let searchButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "Search"), for: .normal)
+        button.backgroundColor = .black
+        return button
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "icon")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .white
+        stackView.spacing = 8
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.backgroundColor = .black
+        return stackView
+    }()
+    
+    private let logoStackView: UIStackView = {
+       let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private let spacerView: UIView = {
+        let view = UIView()
+       
+        return view
+    }()
+    
+    private let categoryList:[String] = ["홈","드라마","예능","영화","스포츠","뉴스","음악"]
+    
+    private lazy var pagingTabBar = PagingTabBar(categoryTitleList:categoryList)
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .black
+        setupUI()
+        
+       
+    }
+    
+    private func setupUI() {
+        self.view.addSubview(logoStackView)
+        
+        [searchButton,iconImageView].forEach {
+            self.buttonStackView.addArrangedSubview($0)
+        }
+        
+       
+        
+        [logoImageView,spacerView,buttonStackView].forEach {
+            self.logoStackView.addArrangedSubview($0)
+        }
+        
+        self.view.addSubview(pagingTabBar)
+        
+        
+        logoStackView.snp.makeConstraints {
+           // $0.top.equalTo(self.view.safeAreaLayoutGuide)
+            $0.top.equalToSuperview().offset(60)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(78)
+        }
+        
+        logoImageView.snp.makeConstraints {
+            $0.width.equalTo(191)
+            $0.height.equalTo(78)
+        }
+        
+        buttonStackView.snp.makeConstraints {
+            $0.width.equalTo(68)
+            $0.height.equalTo(30)
+        }
+        
+        searchButton.snp.makeConstraints {
+            $0.size.equalTo(30)
+        }
+        
+        iconImageView.snp.makeConstraints {
+            $0.size.equalTo(30)
+        }
+        
+        pagingTabBar.snp.makeConstraints {
+            $0.top.equalTo(logoStackView.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+        pagingTabBar.backgroundColor = .black
+ 
     }
 }
+
+
+
+
+
+
+
