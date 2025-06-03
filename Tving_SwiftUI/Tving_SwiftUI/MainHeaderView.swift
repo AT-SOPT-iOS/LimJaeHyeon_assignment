6074b8f689fec176719fc031e24d66ae3dbf0ed7
LimJaeHyeon9298 <limjh9298@gmail.com>
2025-06-03 23:19:33 +0900

    [feat] SwiftUI 파일 생성

---
+++ Tving_SwiftUI/Tving_SwiftUI/MainHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  MainHeaderView.swift
+//  Tving_SwiftUI
+//
+//  Created by 임재현 on 6/3/25.
+//
+
+import SwiftUI
+
+struct MainHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainHeaderView()
+}
